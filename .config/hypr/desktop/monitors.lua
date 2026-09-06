7580f731f6ac05bd15c7c811d296a0bf83aba2fe
Juha Ylikoski <59252246+juha-ylikoski@users.noreply.github.com>
2026-09-06 12:00:19 +0300

    Fix monitor placement

--- .config/hypr/desktop/monitors.lua
+++ .config/hypr/desktop/monitors.lua
@@ -1,16 +1,16 @@
--- Main monitor
-hl.monitor({
-	output = "DP-2",
-	mode = "highrr",
-	position = "auto",
-	scale = 1,
-})
-
 -- Left secondary monitor
 hl.monitor({
 	output = "HDMI-A-1",
 	mode = "preferred",
 	position = "auto-left",
+	scale = 1,
+})
+
+-- Main monitor
+hl.monitor({
+	output = "DP-2",
+	mode = "highrr",
+	position = "0x0",
 	scale = 1,
 })
 
