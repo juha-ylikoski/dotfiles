99e28dfba21a865ee252e6bc841ac81c72bb88fa
Juha Ylikoski <59252246+juha-ylikoski@users.noreply.github.com>
2026-09-06 12:00:45 +0300

    add nvidia env variables

--- .config/hypr/nvidia/env.lua
+++ .config/hypr/nvidia/env.lua
@@ -1,2 +1,5 @@
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("NVD_BACKEND", "direct")
+hl.env("__GL_VRR_ALLOWED", "1")
